07466063df33c2b392c58af92896a8c7c3c36600
Thomas Senter <tom@orphans.co.uk>
2024-05-29 12:30:41 +0100

    Allow publishing of new migration

--- src/ShoppingcartServiceProvider.php
+++ src/ShoppingcartServiceProvider.php
@@ -35,6 +35,7 @@ class ShoppingcartServiceProvider extends ServiceProvider
 
             $this->publishes([
                 __DIR__.'/../database/migrations/0000_00_00_000000_create_shoppingcart_table.php' => database_path('migrations/'.$timestamp.'_create_shoppingcart_table.php'),
+                __DIR__.'/../database/migrations/0000_00_00_000000_create_shoppingcart_uuid_col.php' => database_path('migrations/'.$timestamp.'_create_shoppingcart_uuid_col.php'),
             ], 'migrations');
         }
     }
